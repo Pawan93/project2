0dc43b5ba5c671f8e9c4754ebcff0db98d06e3e1
pawan siwakoti <pawankumarsiwakoti@pawans-iMac.local>
2018-04-09 14:12:35 +1000

    Initial Commit

---
+++ week5/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  week5
+//
+//  Created by pawan kumar siwakoti on 9/4/18.
+//  Copyright © 2018 pawan kumar siwakoti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
